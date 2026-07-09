3fcba7577d7fbef2a31564e73edf12707bda04ef
ali0454hassan <ali0454hassan@gmail.com>
2026-07-09 14:03:24 +0500

    Add heading font configuration

---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  theme: {
+    extend: {
+      fontFamily: {
+        heading: ['"Space Grotesk"', 'sans-serif'],
+      },
+    },
+  },
+};
